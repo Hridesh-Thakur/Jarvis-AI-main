93a1dbfc7c65734ccc70230cae2bbdd17ad802c4
Hridesh Thakur <gws.jaya@gmail.com>
2026-08-04 23:23:28 +0530

    commit 3

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const btn = document.querySelector('.talk');
 const content = document.querySelector('.content');
 
 function speak(text) { 
+  
     const text_speak = new SpeechSynthesisUtterance(text);
 
     text_speak.rate = 1;
